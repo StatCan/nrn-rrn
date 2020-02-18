be26b56ced539e88ca938933dde1c0f4b4974679
KentJacobs <kent.jacobs@canada.ca>
2020-02-18 11:51:33 -0500

    update json pipeline files

--- src/stage_2/test.py
+++
@@ -1,8 +0,0 @@
-import fiona
-import geopandas as gpd
-import os
-import sys
-sys.path.insert(1, os.path.join(sys.path[0], ".."))
-import helpers
-
-dframes = helpers.load_gpkg("../../data/interim/nb.gpkg")
