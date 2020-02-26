5bcd3218185e6da7962e6c30d05ea52dd7bd37a4
KentJacobs <kent.jacobs@canada.ca>
2020-02-26 16:57:06 -0500

    testing stage 1 definition

--- pipelines/run.sh
+++ pipelines/run.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
-export nrn_source=`ls /pfs/input/`
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+
+ls /pfs/out/data/raw
+export nrn_source=`ls /pfs/out/data/raw`
 
 cd $nrn_working_dir
 
-if [ "$nrn_working_dir" == "src/stage_1" ]
+if [ "$nrn_working_dir" = "/nrn-app/src/stage_1" ]
 then
-	cp -r /pfs/input/. ../../data/raw	
+	cp -r /pfs/out/data /nrn-app/data
 	python3 stage_1.py $nrn_source
+	cp -r /nrn-app/data /pfs/out/data
 
-elif [ "$nrn_working_dir" == "src/stage_2" ]
+elif [ "$nrn_working_dir" = "/nrn-app/src/stage_2" ]
 then
 	python3 stage_2.py $nrn_source
-	cp -r ../../data/interim/$nrn_source.gpkg /pfs/output/	
 fi
