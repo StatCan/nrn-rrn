7df2c8098d17f3b6f7b9b11740373f7ee1f0da85
KentJacobs <kent.jacobs@canada.ca>
2020-02-27 19:05:09 -0500

    debugging stage 1

---
+++ scripts/copy-data.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# mkdir -p /tmp/stage_0_nb
+
+cp -r /nrn-app/data /pfs/out/
--- scripts/dl-data.sh
+++ scripts/dl-data.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-mkdir -p data/raw/${nrn_pr_code} && 
+mkdir -p data/raw/${nrn_pr_code}
+
 ogr2ogr -f 'ESRI Shapefile' -t_srs EPSG:4617 data/raw/${nrn_pr_code} /vsizip//vsicurl/${nrn_url}
