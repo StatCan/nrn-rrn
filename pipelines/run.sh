5505d0d128058db31db4f4c9a68872a8495b2535
KentJacobs <kent.jacobs@canada.ca>
2020-03-02 15:16:04 -0500

    execute stages 1 and 2 output

--- pipelines/run.sh
+++ pipelines/run.sh
@@ -2,19 +2,22 @@
 
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
-
-ls /pfs/out/data/raw
-export nrn_source=`ls /pfs/out/data/raw`
+export nrn_source=`ls /pfs/data/data/raw`
 
 cd $nrn_working_dir
 
 if [ "$nrn_working_dir" = "/nrn-app/src/stage_1" ]
 then
-	cp -r /pfs/out/data /nrn-app/data
+	cp -r /pfs/data/data/raw/$nrn_source /nrn-app/data/raw
 	python3 stage_1.py $nrn_source
-	cp -r /nrn-app/data /pfs/out/data
+	cp -r /nrn-app/data/interim/. /pfs/out/data/
+	mkdir -p /pfs/out/data/raw
+	cp -r /nrn-app/data/raw/. /pfs/out/data/raw/
 
 elif [ "$nrn_working_dir" = "/nrn-app/src/stage_2" ]
 then
+	cp /pfs/data/data/*.gpkg /nrn-app/data/interim
+	service postgresql start
 	python3 stage_2.py $nrn_source
+	cp -r /nrn-app/data/interim/. /pfs/out/data/
 fi
