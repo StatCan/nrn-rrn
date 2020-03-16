2e62f70526c4995ae9dd9ffa9c31577446f70b2a
KentJacobs <kent.jacobs@canada.ca>
2020-03-16 15:47:53 -0400

    change FetchData PR environment variable name

--- scripts/dl-data.sh
+++ scripts/dl-data.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-mkdir -p data/raw/${nrn_pr_code}
+mkdir -p data/raw/${nrn_pr_dir}
 
-ogr2ogr -f 'ESRI Shapefile' -t_srs EPSG:4617 data/raw/${nrn_pr_code} /vsizip//vsicurl/${nrn_url}
+ogr2ogr -f 'ESRI Shapefile' -t_srs EPSG:4617 data/raw/${nrn_pr_dir} /vsizip//vsicurl/${nrn_url}
