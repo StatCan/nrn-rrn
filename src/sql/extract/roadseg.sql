0c53befca3f4fb8cff84581581e5cd7491cb710f
Jesse Stewart <jesse-stewart18@hotmail.com>
2022-10-25 15:59:30 -0300

    [1] simplified crn data model - changed sql accordingly.

--- src/sql/extract/roadseg.sql
+++ src/sql/extract/roadseg.sql
@@ -83,7 +83,11 @@ SELECT REPLACE(nrn.segment_id::text, '-', '') AS segment_id,
        provider_lookup.value_en AS provider,
        nrn.creation_date AS credate,
        nrn.revision_date AS revdate,
-       nrn.segment_type,
+       nrn.segment_type AS segment_type,
+       nrn.exit_number AS exitnbr,
+       nrn.speed AS speed,
+       nrn.number_of_lanes AS nbrlanes,
+       nrn.road_jurisdiction AS roadjuris,
        nrn.geometry,
        strplaname_l_acquisition_technique_lookup.value_en AS strplaname_l_acqtech,
        strplaname_l_provider_lookup.value_en AS strplaname_l_provider,
@@ -100,7 +104,6 @@ SELECT REPLACE(nrn.segment_id::text, '-', '') AS segment_id,
        strplaname_r_place_type_lookup.value_en AS strplaname_r_placetype,
        strplaname_r_province_lookup.value_en AS strplaname_r_province,
        closing_period_lookup.value_en AS closing,
-       exit_number.exit_number AS exitnbr,
        functional_road_class_lookup.value_en AS roadclass,
        CASE road_surface_type_lookup.value_en
          WHEN 'Unknown' THEN 'Unknown'
@@ -161,8 +164,6 @@ SELECT REPLACE(nrn.segment_id::text, '-', '') AS segment_id,
        addrange_r_last_house_number_type_lookup.value_en AS addrange_r_hnumtypl,
        addrange_r_house_number_structure_lookup.value_en AS addrange_r_hnumstr,
        addrange_r_reference_system_indicator_lookup.value_en AS addrange_r_rfsysind,
-       number_of_lanes.number_of_lanes AS nbrlanes,
-       road_jurisdiction.road_jurisdiction AS roadjuris,
        route_name_1.route_name_en AS rtename1en,
        route_name_1.route_name_fr AS rtename1fr,
        route_name_2.route_name_en AS rtename2en,
@@ -176,7 +177,6 @@ SELECT REPLACE(nrn.segment_id::text, '-', '') AS segment_id,
        route_number_3.route_number AS rtnumber3,
        route_number_4.route_number AS rtnumber4,
        route_number_5.route_number AS rtnumber5,
-       speed.speed AS speed,
        street_name_l.street_name_concatenated AS l_stname_c,
        strplaname_l_street_direction_prefix_lookup.value_en AS strplaname_l_dirprefix,
        strplaname_l_street_type_prefix_lookup.value_en AS strplaname_l_strtypre,
@@ -249,11 +249,6 @@ FROM
    WHERE segment_source.strplaname_l_province = {{ source_code }} OR segment_source.strplaname_r_province = {{ source_code }}) nrn
 
 -- Join with all linked datasets.
-LEFT JOIN public.closing_period closing_period ON nrn.segment_id = closing_period.segment_id
-LEFT JOIN public.exit_number exit_number ON nrn.segment_id = exit_number.segment_id
-LEFT JOIN public.functional_road_class functional_road_class ON nrn.segment_id = functional_road_class.segment_id
-LEFT JOIN public.road_surface_type road_surface_type ON nrn.segment_id = road_surface_type.segment_id
-
 LEFT JOIN
   (SELECT structure_link.segment_id,
           structure.*
@@ -261,11 +256,8 @@ LEFT JOIN
    LEFT JOIN public.structure structure ON structure_link.structure_id = structure.structure_id) structure_source
 ON nrn.segment_id = structure_source.segment_id
 
-LEFT JOIN public.traffic_direction traffic_direction ON nrn.segment_id = traffic_direction.segment_id
 LEFT JOIN public.address_range addrange_l ON nrn.segment_id_left = addrange_l.segment_id
 LEFT JOIN public.address_range addrange_r ON nrn.segment_id_right = addrange_r.segment_id
-LEFT JOIN public.number_of_lanes number_of_lanes ON nrn.segment_id_right = number_of_lanes.segment_id
-LEFT JOIN public.road_jurisdiction road_jurisdiction ON nrn.segment_id_right = road_jurisdiction.segment_id
 LEFT JOIN route_name_1 ON nrn.segment_id_right = route_name_1.segment_id
 LEFT JOIN route_name_2 ON nrn.segment_id_right = route_name_2.segment_id
 LEFT JOIN route_name_3 ON nrn.segment_id_right = route_name_3.segment_id
@@ -275,7 +267,6 @@ LEFT JOIN route_number_2 ON nrn.segment_id_right = route_number_2.segment_id
 LEFT JOIN route_number_3 ON nrn.segment_id_right = route_number_3.segment_id
 LEFT JOIN route_number_4 ON nrn.segment_id_right = route_number_4.segment_id
 LEFT JOIN route_number_5 ON nrn.segment_id_right = route_number_5.segment_id
-LEFT JOIN public.speed speed ON nrn.segment_id_right = speed.segment_id
 LEFT JOIN street_name street_name_l ON nrn.segment_id_left = street_name_l.segment_id
 LEFT JOIN street_name street_name_r ON nrn.segment_id_right = street_name_r.segment_id
 
@@ -290,11 +281,11 @@ LEFT JOIN public.acquisition_technique_lookup strplaname_r_acquisition_technique
 LEFT JOIN public.provider_lookup strplaname_r_provider_lookup ON nrn.strplaname_r_provider = strplaname_r_provider_lookup.code
 LEFT JOIN public.place_type_lookup strplaname_r_place_type_lookup ON nrn.strplaname_r_place_type = strplaname_r_place_type_lookup.code
 LEFT JOIN public.province_lookup strplaname_r_province_lookup ON nrn.strplaname_r_province = strplaname_r_province_lookup.code
-LEFT JOIN public.closing_period_lookup closing_period_lookup ON closing_period.closing_period = closing_period_lookup.code
-LEFT JOIN public.functional_road_class_lookup functional_road_class_lookup ON functional_road_class.functional_road_class = functional_road_class_lookup.code
-LEFT JOIN public.road_surface_type_lookup road_surface_type_lookup ON road_surface_type.road_surface_type = road_surface_type_lookup.code
+LEFT JOIN public.closing_period_lookup closing_period_lookup ON nrn.closing_period = closing_period_lookup.code
+LEFT JOIN public.functional_road_class_lookup functional_road_class_lookup ON nrn.functional_road_class = functional_road_class_lookup.code
+LEFT JOIN public.road_surface_type_lookup road_surface_type_lookup ON nrn.road_surface_type = road_surface_type_lookup.code
 LEFT JOIN public.structure_type_lookup structure_type_lookup ON structure_source.structure_type = structure_type_lookup.code
-LEFT JOIN public.traffic_direction_lookup traffic_direction_lookup ON traffic_direction.traffic_direction = traffic_direction_lookup.code
+LEFT JOIN public.traffic_direction_lookup traffic_direction_lookup ON nrn.traffic_direction = traffic_direction_lookup.code
 LEFT JOIN public.house_number_type_lookup addrange_l_first_house_number_type_lookup ON addrange_l.first_house_number_type = addrange_l_first_house_number_type_lookup.code
 LEFT JOIN public.house_number_type_lookup addrange_l_last_house_number_type_lookup ON addrange_l.last_house_number_type = addrange_l_last_house_number_type_lookup.code
 LEFT JOIN public.house_number_structure_lookup addrange_l_house_number_structure_lookup ON addrange_l.house_number_structure = addrange_l_house_number_structure_lookup.code
